a487448b7a6af2e9a4d445c03739a5930719d24f
Cassandra Wilcox <csndra0504@gmail.com>
2016-04-19 18:53:27 -0400

    chore: base component markup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const App = () => (
-  <div>Hello world</div>
+  	<form action="">
+		<div>
+			<label for="name">Name</label>
+			<input type="text" />
+		</div>
+		<div>
+			<label for="number">Card Number</label>
+			<input type="text" />
+		</div>
+		<div>
+			<label for="expiration-month">Expiration</label>
+			<select name="" id="">
+				<option value="1">(1) January</option>
+				<option value="1">(2) February</option>
+				<option value="1">(3) March</option>
+			</select>
+			<select name="" id="">
+				<option value="2016">2016</option>
+				<option value="2017">2017</option>
+				<option value="2018">2018</option>
+				<option value="2019">2019</option>
+				<option value="2020">2020</option>
+			</select>
+		</div>
+		<div>
+			<button>Submit</button>
+		</div>
+	</form>
 );
 
 module.exports = App;
