51a0d8ae4fd5403d7fb5bb545dfe95e4ac0a3621
Cassandra Wilcox <csndra0504@gmail.com>
2016-04-19 19:03:54 -0400

    chore: include bootstraps-sass

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,9 @@
 // TEMPLATES
 require.context('./', true, /\.html$/);
 
+//Bootstrap
+require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
