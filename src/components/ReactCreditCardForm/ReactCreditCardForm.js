c61b709f0de090f6e4e8c7d61b62939fbb4aa565
Cassandra Wilcox <csndra0504@gmail.com>
2016-04-19 20:37:03 -0400

    chore: apply Field component to all form inputs

--- src/components/ReactCreditCardForm/ReactCreditCardForm.js
+++ src/components/ReactCreditCardForm/ReactCreditCardForm.js
@@ -13,7 +13,7 @@ class ReactCreditCardForm extends React.Component{
 
   handleSubmit(cc) {
     // let { dispatch } = this.props;
-    let { handleSubmit } = this.props;
+    let { handleSubmit } = this.props; //same as var handleSubmit = this.props.handleSubmit;
 
     console.log("cc", cc)
 
@@ -29,44 +29,51 @@ class ReactCreditCardForm extends React.Component{
 		return(
 		  	<Form className="row" model="cc" onSubmit={(cc) => this.handleSubmit(cc)}>
 	  			<div className="form-group">
-
 			  		<Field model="cc.name" className="col-md-12">
-							<label for="name">Name</label>
-							<input type="text" className="form-control"/>
-						</Field>
+						<label for="name">Name</label>
+						<input type="text" className="form-control"/>
+					</Field>
 	  			</div>
 
 				<div className="col-md-12">
 					<div className="form-group">
-						<label for="number">Card Number</label>
-						<input type="text" className="form-control" onChange={this.handleNumberChange}/>
+						<Field model="cc.number">
+							<label for="number">Card Number</label>
+							<input type="text" className="form-control" onChange={this.handleNumberChange}/>
+						</Field>
 					</div>
 				</div>
 				<div className="col-md-6">
 					<label for="expiration-month">Expiration</label>
 					<div className="row">
 						<div className="col-md-6">
-							<select name="" id="" className="form-control">
-								<option value="1">(1) January</option>
-								<option value="1">(2) February</option>
-								<option value="1">(3) March</option>
-							</select>
+							<Field model="cc.expMonth">
+								<select name="" id="" className="form-control">
+									<option value="1">(1) January</option>
+									<option value="2">(2) February</option>
+									<option value="3">(3) March</option>
+								</select>
+							</Field>
 						</div>
 						<div className="col-md-6">
-							<select name="" id="" className="form-control col-md-6">
-								<option value="2016">2016</option>
-								<option value="2017">2017</option>
-								<option value="2018">2018</option>
-								<option value="2019">2019</option>
-								<option value="2020">2020</option>
-							</select>
+							<Field model="cc.expYear">
+								<select name="" id="" className="form-control col-md-6">
+									<option value="2016">2016</option>
+									<option value="2017">2017</option>
+									<option value="2018">2018</option>
+									<option value="2019">2019</option>
+									<option value="2020">2020</option>
+								</select>
+							</Field>
 						</div>
 					</div>
 				</div>
 				<div className="col-md-6">
 					<div className="form-group">
-						<label for="name">CVC Code</label>
-						<input type="text" className="form-control"/>
+						<Field model="cc.cvc">
+							<label for="name">CVC Code</label>
+							<input type="text" className="form-control"/>
+						</Field>
 					</div>
 				</div>
 				<div className="col-md-12">
