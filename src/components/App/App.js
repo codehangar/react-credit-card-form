6d060a5221fd8e2048e21adebf436e56d6b1de4e
Ian Grail <grailians@gmail.com>
2016-04-19 20:07:16 -0400

    chore: update App to include redux form stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactCreditCardForm from '../ReactCreditCardForm/ReactCreditCardForm';
+import UserForm from '../UserForm/UserForm';
+
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import store from '../../store.js';
+
+const handleSubmit = (user) => {
+  // actions.submit('user', somePromise)
+  console.log("App.js user", user)
+}
 
 const App = () => (
-	<ReactCreditCardForm data={{test: 'hello world'}}/>
+  <Provider store={ store }>
+    <div>
+      <ReactCreditCardForm data={{test: 'hello world'}} handleSubmit={(user) => handleSubmit(user)}/>
+      <UserForm handleSubmit={(user) => handleSubmit(user)}/>
+    </div>
+  </Provider>
 );
 
 module.exports = App;
